aefb9e99599a670eef1ac62907b208811d9e3ebf
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-03-16 09:22:09 +0530

    Added dynamic detail cards below the singel product page

--- src/Components/CartProduct/CartProduct.jsx
+++ src/Components/CartProduct/CartProduct.jsx
@@ -80,7 +80,7 @@ const CartProduct = ({
       </div>
       <div className="cart-product-pricing">
         <h2>
-          <FaRupeeSign size={window.innerWidth > 1280 ? 20 : 10} />
+          <FaRupeeSign size={window.innerWidth >= 1280 ? 20 : 10} />
           {product?.price}
         </h2>
         <div className="cart-quantity-setter">
